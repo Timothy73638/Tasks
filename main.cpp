edec836708cc2789099161d684b2b7efe21fb4db
Timothy73638 <eddalitima1@gmail.com>
2025-09-21 21:19:33 +0300

    Task number 3

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+   const char* str = "Line";
+   int length = strlen(str);
+   cout << "the lenght of the line: "<< length << endl;
+   return 0;
+}
