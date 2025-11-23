dce422282ae055885302f440cafe2adf7f52b800
Timothy73638 <eddalitima1@gmail.com>
2025-11-23 20:46:45 +0000

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int add(int a, int b) {
+return a + b;
+}
---
+++ main2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main(){
+
+cout << "5 + 5 = " << add(5, 5) << endl;
+return 0;
+}
